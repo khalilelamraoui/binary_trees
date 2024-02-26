f1cec9614748f81d81346c19eba7503ce6038928
khalilelamraoui <khalilelam16@gmail.com>
2024-02-26 19:15:43 +0100

    added 4 tasks

--- binary_trees.h
+++ binary_trees.h
@@ -5,19 +5,20 @@
 #include<stdlib.h>
 
 /**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+* struct binary_tree_s - Binary tree node
+*
+* @n: Integer stored in the node
+* @parent: Pointer to the parent node
+* @left: Pointer to the left child node
+* @right: Pointer to the right child node
+*/
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
@@ -29,5 +30,37 @@ typedef struct binary_tree_s avl_t;
 typedef struct binary_tree_s heap_t;
 
 void binary_tree_print(const binary_tree_t *);
+
+/* Function Prototypes */
+/*
+* binary_tree_node - creates a binary tree node
+* @parent: pointer to the parent node of the node to create
+* @value: value to put in the new node
+* Return: pointer to the new node, or NULL on failure
+*/
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+/**
+* binary_tree_insert_left - inserts a node as the left-child of another node
+* @parent: pointer to the node to insert the left-child in
+* @value: value to store in the new node
+* Return: pointer to the created node, or NULL on failure or if parent is NULL
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+
+/**
+* binary_tree_insert_right - inserts a node as the right-child of another node
+* @parent: pointer to the node to insert the right-child in
+* @value: value to store in the new node
+* Return: pointer to the created node, or NULL on failure or if parent is NULL
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+
+/**
+* binary_tree_delete - deletes an entire binary tree
+* @tree: pointer to the root node of the tree to delete
+* Return: void
+*/
+void binary_tree_delete(binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
